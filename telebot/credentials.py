c4823fb0762e96b0d1983671c2a0fc06707dabf0
Everton-Luiss <elssufpa@gmail.com>
2020-08-23 16:20:50 -0300

    Token

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
-bot_token = "AAHGrYpN5YIzROkBTUf08xll_tFT7h6KZ-Q"
+bot_token = "AAENzTPkvBmYr0ZefQY9HPcdPZmfDyI37cA"
 bot_user_name = "bot username"
-URL = "https://final-robo2.herokuapp.com/"
+URL = "xx"
