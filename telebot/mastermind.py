3a7934e466d33058e8ac2e3bd7a4918e7582fbb1
Everton-Luiss <elssufpa@gmail.com>
2020-08-23 17:12:59 -0300

    Update mastermind.py

--- telebot/mastermind.py
+++ telebot/mastermind.py
@@ -7,6 +7,7 @@ import random
 import os
 from flask import Flask, request, make_response
 import json
+from telebot.credentials import bot_token
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
@@ -697,7 +698,7 @@ def cancel(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Pena que você já vai. \n\nSe precisar de mim é só chamar!!!")
     return ConversationHandler.END
 def main():
-    TOKEN='1368978547:AAEoYdgxdm586q7tcF1xQT3OpL3SBZBNLT0'
+    TOKEN=bot_token
     updater = Updater(token=TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
