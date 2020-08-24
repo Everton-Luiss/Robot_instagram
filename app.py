64fcd61c98c77a8b7a9615517b2ba96de9ba7c6b
Everton-Luiss <elssufpa@gmail.com>
2020-08-24 11:00:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -25,14 +25,14 @@ def respond():
 
     return 'ok'
 
-@app.route('/setwebhook', methods=['GET', 'POST'])
+'''@app.route('/setwebhook', methods=['GET', 'POST'])
 def set_webhook():
     s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
     if s:
         return "webhook setup ok"
     else:
         return "webhook setup failed"
-
+'''
 @app.route('/')
 def index():
     return '...'
