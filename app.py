24035698be733f48a7505eec1b0542a496089e2a
Everton-Luiss <elssufpa@gmail.com>
2020-08-24 11:47:12 -0300

    s

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 import telegram
 from telebot.credentials import bot_token, bot_user_name,URL
-from telebot.mastermind import get_response
+from telebot.mastermind import start
 
 global bot
 global TOKEN
@@ -14,14 +14,18 @@ def respond():
   
     update = telegram.Update.de_json(request.get_json(force=True), bot)
 
-    chat_id = update.message.chat.id
-    msg_id = update.message.message_id
+    #chat_id = update.message.chat.id
+    #msg_id = update.message.message_id
 
     text = update.message.text.encode('utf-8').decode()
     print("got text message :", text)
+    if text == "/start":
+        start(text)
+        # send the welcoming message
+        #bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
 
-    response = get_response(text)
-    bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
+   # response = get_response(text)
+    #bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
 
     return 'ok'
 
