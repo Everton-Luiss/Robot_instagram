9d998b83512ddd24c381d14ed8ceaa4790f93902
Everton-Luiss <elssufpa@gmail.com>
2020-08-24 14:14:56 -0300

    Update credentials.py

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
-bot_token = "11368978547:AAEoYdgxdm586q7tcF1xQT3OpL3SBZBNLT0"
+bot_token = '11368978547:AAEoYdgxdm586q7tcF1xQT3OpL3SBZBNLT0'
 bot_user_name = "growupInsta_bot"
-URL = "https://robot-instagran.herokuapp.com/"
+URL = 'https://robot-instagran.herokuapp.com/'
