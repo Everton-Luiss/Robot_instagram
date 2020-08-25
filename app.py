a511b3f98f9222fe73096a5b014f5242f1192312
Everton-Luiss <elssufpa@gmail.com>
2020-08-25 18:27:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,18 +10,8 @@ app = Flask(__name__)
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 #@app.route('/respond', methods=['POST'])
 def respond():
-    update = telegram.Update.de_json(request.get_json(force=True), bot)
-    chat_id = update.message.chat.id
-    msg_id = update.message.message_id
-    text = update.message.text.encode('utf-8').decode()
-    print("got text message :", text)
-    if text == '/start':
-        print('iniciando')
-        return main
-    else:
-        error = 'Digite /start'
-        bot.sendMessage(chat_id=chat_id, text=error, reply_to_message_id=msg_id)
-    return 'Ok go!'
+    print('iniciando')
+    return main
 
 @app.route('/')
 def index():
