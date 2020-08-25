26491b05f1b6c8d419da6b181b824dd8cf3a098f
Everton-Luiss <elssufpa@gmail.com>
2020-08-25 18:30:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import telegram
 #from telebot.credentials import bot_token, bot_user_name,URL
 from telebot.mastermind import main
 
-TOKEN = '11368978547:AAEoYdgxdm586q7tcF1xQT3OpL3SBZBNLT0'
-URL = 'https://robot-instagran.herokuapp.com/'
+#TOKEN = '11368978547:AAEoYdgxdm586q7tcF1xQT3OpL3SBZBNLT0'
+#URL = 'https://robot-instagran.herokuapp.com/'
 
 app = Flask(__name__)
-@app.route('/{}'.format(TOKEN), methods=['POST'])
-#@app.route('/respond', methods=['POST'])
+#@app.route('/{}'.format(TOKEN), methods=['POST'])
+@app.route('/respond', methods=['POST'])
 def respond():
     print('iniciando')
     return main
