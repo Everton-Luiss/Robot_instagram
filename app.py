20ebd0757362f5ccfc7026615912279ab782b2d3
Everton-Luiss <elssufpa@gmail.com>
2020-08-25 17:42:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,19 @@ from telebot.mastermind import main
 app = Flask(__name__)
 @app.route('/respond', methods=['POST'])
 def respond():
-    print('iniciando')
-    return main
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    chat_id = update.message.chat.id
+    msg_id = update.message.message_id
+    text = update.message.text.encode('utf-8').decode()
+    print("got text message :", text)
+    if text == '/start':
+        print(iniciando)
+        return main
+    else:
+        bot_error = 'Digite /start'
+        bot.sendMessage(chat_id=chat_id, text=bot_error, reply_to_message_id=msg_id)
+
+    return 'OK go!'
 
 @app.route('/')
 def index():
