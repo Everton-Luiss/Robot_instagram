98a8cb81016dd12b57e244b9d9979ab420f917e7
Everton-Luiss <elssufpa@gmail.com>
2020-08-25 17:50:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request
 import telegram
-from telebot.credentials import bot_token, bot_user_name,URL
+#from telebot.credentials import bot_token, bot_user_name,URL
 from telebot.mastermind import main
+
+TOKEN = '11368978547:AAEoYdgxdm586q7tcF1xQT3OpL3SBZBNLT0'
+URL = 'https://robot-instagran.herokuapp.com/'
 
 app = Flask(__name__)
 @app.route('/respond', methods=['POST'])
@@ -19,6 +22,14 @@ def respond():
         bot.sendMessage(chat_id=chat_id, text=bot_error, reply_to_message_id=msg_id)
 
     return 'OK go!'
+
+@app.route('/setwebhook', methods=['GET', 'POST'])
+def set_webhook():
+    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    if s:
+        return "webhook setup ok"
+    else:
+        return "webhook setup failed"
 
 @app.route('/')
 def index():
