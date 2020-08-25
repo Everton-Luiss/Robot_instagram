4559759d8a68cf30c673fca3729dc40596a4db3e
Everton-Luiss <elssufpa@gmail.com>
2020-08-25 17:55:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,6 @@ def respond():
     print(iniciando)
     return main
 
-
-@app.route('/setwebhook', methods=['GET', 'POST'])
-def set_webhook():
-    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
-    if s:
-        return "webhook setup ok"
-    else:
-        return "webhook setup failed"
-
 @app.route('/')
 def index():
     return 'Robot-Instagram'
