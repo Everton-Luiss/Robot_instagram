cb1fe8a1c5b9b04fdcaa03a3642f924b4b50a978
Everton-Luiss <elssufpa@gmail.com>
2020-08-25 18:09:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,28 @@ TOKEN = '11368978547:AAEoYdgxdm586q7tcF1xQT3OpL3SBZBNLT0'
 URL = 'https://robot-instagran.herokuapp.com/'
 
 app = Flask(__name__)
-#@app.route('/{}'.format(TOKEN), methods=['POST'])
-@app.route('/respond', methods=['POST'])
+@app.route('/{}'.format(TOKEN), methods=['POST'])
+#@app.route('/respond', methods=['POST'])
 def respond():
-    print('iniciando')
-    return main
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    chat_id = update.message.chat.id
+    msg_id = update.message.message_id
+    text = update.message.text.encode('utf-8').decode()
+    print("got text message :", text)
+    if text=='/start':
+        print('iniciando')
+        return main
+    else:
+        error='Digite /start'
+        bot.sendMessage(chat_id=chat_id, text=error, reply_to_message_id=msg_id)
+    return 'Ok go!'
+
+@app.route('/setwebhook', methods=['GET', 'POST'])
+def set_webhook():
+    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    if s:
+        return "webhook setup ok"
+    else:
 
 @app.route('/')
 def index():
