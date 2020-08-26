bbcfd631a271967d04aa681b38dce40a8f8cca77
Everton-Luiss <elssufpa@gmail.com>
2020-08-26 10:10:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,14 +13,13 @@ def respond():
     update = telegram.Update.de_json(request.get_json(force=True), bot)
 
     chat_id = update.message.chat.id
-    msg_id = update.message.message_id
 
     # Telegram understands UTF-8, so encode text for unicode compatibility
     text = update.message.text.encode('utf-8').decode()
     print("got text message :", text)
 
-    response = main(text)
-    bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
+    response = main()
+    bot.sendMessage(chat_id=chat_id, text=response)
 
     return 'ok'
 
